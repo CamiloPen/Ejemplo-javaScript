65f2a9b5238ce6ee6d1c640b1c45a006658406b7
Juan Camilo Penagos Rincon <76062268+CamiloPen@users.noreply.github.com>
2025-01-12 22:10:52 -0500

    Add files via upload

---
+++ Calculadora-Areas/js/main.js
@@ -0,0 +1,53 @@
+function menu() {
+    return opc = prompt("1.Area de un rectangulo.\n2.Area de un triangulo.\n3.Area de un circulo.\n4.Area de un rombo.\n5.Salir\nElija una opción (1-5)")
+}
+
+function areaCirculo() {
+    const radio = prompt("Area del Circulo\nPor favor digite el radio del circulo")
+    return resultado = 3.1416 * radio * radio
+}
+
+function areas(figura) {
+    let base = 0
+    let altura = 0
+    let mensaje1 = "base"
+    let mensaje2 = "altura"
+
+    if (figura == "rombo") {
+        mensaje1 = "diagonal 1"
+        mensaje2 = "diagonal 2"
+    }
+    base = prompt("Area del " + figura + "\nPor favor digite La base del " + figura)
+    altura = prompt("Area del " + figura + "\nPor favor digite La altura del " + figura)
+    resultado = base * altura
+
+    if (figura == "triangulo") {
+        resultado /= 2
+    }
+
+    return resultado
+}
+
+alert("BIENVENIDO\nCALCULADORA DE AREAS")
+while (true) {
+    const opc = menu()
+    let resultado = 0
+    switch (opc) {
+        case "1":
+            resultado = areas("rectangulo")
+            alert("El area del rectangulo es: " + resultado)
+        case "2":
+            resultado = areas("triangulo")
+            alert("El area del triangulo es: " + resultado)
+        case "3":
+            resultado = areaCirculo()
+            alert("El area del Circulo es: " + resultado)
+        case "4":
+            resultado = areas("rombo")
+            alert("El area del rombo es: " + resultado)
+        case "5":
+            break;
+        default:
+            alert("Opción incorrecta")
+    }
+}
