7a6be3a9536335e5bd327cf0ea2942ade295014b
Juan Camilo Penagos Rincon <76062268+CamiloPen@users.noreply.github.com>
2025-01-13 18:44:50 -0500

    Add files via upload

--- picas-fijas/js/main.js
+++ picas-fijas/js/main.js
@@ -1,13 +1,53 @@
-let numFijo = Math.floor(Math.random() * 9999);
-console.log(numFijo)
-let numero = prompt("Digite un número de 4 digitos")
-let fijas = 0
-let picas = 0
-
-for (let i = 0; i < 4; i++) {
-    if (numero[i] == numFijo[i]){
-        fijas += 1
+function random() {
+    while (true) {
+        let num = Math.floor(Math.random() * 9999).toString();
+        if (num.length == 4){
+            num = comprobar(num)
+            if (num) {
+                return num
+            }
+        }
     }
 }
 
-console.log(fijas)
+function comprobar(num) {
+    let repetido = false
+    for (let i = 0; i <= 3; i++) {
+        const contador = num.search(num[i])
+        if (contador != i){
+            repetido = true
+        }
+    }
+    if (repetido == false){
+        return num
+    }
+}
+
+const numFijo = random()
+while (true) {
+    let fijas = 0
+    let picas = 0
+    console.log(numFijo)
+    let numero = prompt("Digite un número de 4 digitos distintos")
+    if (numero.length > 3) {
+        numero = comprobar(numero)
+        if (numero == numFijo) {
+            alert("Felicitaciones el numero es: " + numFijo)
+            break
+        } else {
+            for (let i = 0; i <= 3; i++) {
+                if (numFijo[i] == numero[i]) {
+                    fijas += 1
+                } else if (numFijo.search(numero[i]) != -1) {
+                    picas += 1
+                }
+            }
+        }
+        alert("Su numero es: " + numero + "\nTiene " + fijas + " fijas" + "\nTiene " + picas + " picas")
+    } else {
+        alert("Por favor dijete 4 digitos distintos")
+    }
+    if (numero === undefined){
+        alert("Por favor dijete 4 digitos distintos")
+    }
+}
